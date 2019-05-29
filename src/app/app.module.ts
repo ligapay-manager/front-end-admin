d4e5826a0b3defb34c43e4bdc6ca713e40afdab3
Vinicius Zorzanelli <vsz.vinicius@gmail.com>
2019-05-28 23:44:26 -0300

    Inclusão menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+import {ROUTES} from './app.routes';
+import {RouterModule} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES),
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+export const ROUTES: Routes = [
+  {path: '', component: LoginPageComponent}
+];
