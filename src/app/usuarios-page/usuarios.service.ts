1dca8dbc487777692907687041e5b3773e5acabd
Vinicius Zorzanelli <vinizorza@hotmail.com>
2019-06-07 08:46:02 -0300

    mudança header 'aplication/graphql' para 'aplication/json'

--- src/app/usuarios-page/usuarios.service.ts
+++ src/app/usuarios-page/usuarios.service.ts
@@ -1,37 +1,31 @@
-import {Usuario} from './usuario.model';
-import {LIGAPAY_API} from '../app.api';
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {HttpHeaders} from '@angular/common/http';
+import { Usuario } from './usuario.model';
+import { LIGAPAY_API } from '../app.api';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
 export class UsuariosService {
 
-    
-
-    constructor(private http: HttpClient){
-
-    }
+    constructor(private http: HttpClient) { }
 
     httpOptions = {
         headers: new HttpHeaders({
-            'Content-Type':  'application/graphql'
+            'Content-Type': 'application/json'
         })
     };
 
-    
-
-    userToken = "aaa";
+    userToken = "";
 
     listarUsuarios(): Usuario[] {
 
-        this.http.post('http://ligapay.tk/', 
-        `mutation{
-            login(email: "serra.santos.s@gmail.com", password:"A151102!"){
+        this.http.post('http://ligapay.tk/', {
+            query: `mutation ($email: String!, $password: String!) {
+            login(email: $email, password: $password){
               token
               user{
                 wallet{
@@ -40,60 +34,59 @@ export class UsuariosService {
               }
             }
           }`,
-          this.httpOptions    
+            variables: {
+                email: "serra.santos.s@gmail.com",
+                password: "A151102!"
+            }
+        },
+            this.httpOptions
         ).subscribe(
             result => {
                 this.userToken = result['data']['login']['token'];
-                console.log(this.userToken);
 
                 const httpOptions2 = {
                     headers: new HttpHeaders({
-                        'Content-Type':  'application/graphql',
+                        'Content-Type': 'application/json',
                         'Authorization': 'Bearer ' + this.userToken
                     })
-                };  
+                };
 
-                console.log(httpOptions2.headers);    
-
-                this.http.post('http://ligapay.tk/', 
-                `query{
-                    users{
-                      email
-                    }
-                  }`,
-                httpOptions2    
+                this.http.post('http://ligapay.tk/',
+                    {query: 
+                        `query{
+                            users{
+                            email
+                            }
+                        }`
+                    },
+                    httpOptions2
                 ).subscribe(
                     result => {
                         console.log(result);
                     }
                 )
-
-
-
             }
         )
 
-        console.log(this.userToken);
-
         const usuarios: Usuario[] = [
-            {id: 1, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 2, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 3, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 4, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 5, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 6, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 7, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 8, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 9, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 10, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 11, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 12, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 13, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 14, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 15, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 16, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},
-            {id: 17, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998},    
-            ];
+            { id: 1, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 2, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 3, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 4, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 5, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 6, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 7, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 8, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 9, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 10, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 11, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 12, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 13, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 14, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 15, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 16, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+            { id: 17, nome: 'Vinicius Zorzanelli', email: 'vinicius@gmail.com', nomeTime: 'Arostoles', montanteCarteira: 15.9, pontuacao: 998 },
+        ];
 
         return usuarios;
     }
